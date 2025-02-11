1d92454dbb4c4b9e25088535f9c8e536c0d42e8b
nirajchopra <nirajch0700@gmail.com>
2025-02-11 15:35:07 +0530

    Spring-core

---
+++ Spring-core/src/main/java/com/rays/autowire/bytype/TestAutowirebyType.java
@@ -0,0 +1,15 @@
+package com.rays.autowire.bytype;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestAutowirebyType {
+	
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("autowire-by-type.xml");
+		
+		UserService userService = (UserService) context.getBean("userService");
+		userService.testAdd();
+	}
+
+}
