79a712fd55bfbcd64fab8286bc6b954e2cd6970c
nirajchopra <nirajch0700@gmail.com>
2025-02-11 15:21:13 +0530

    Spring-core

---
+++ Spring-core/src/main/java/com/rays/primary/TwitterService.java
@@ -0,0 +1,9 @@
+package com.rays.primary;
+
+public class TwitterService implements MessageService{
+	
+	public void sendMessage(String message) {
+		System.out.println("Sending twitter: " + message);
+	}
+
+}
